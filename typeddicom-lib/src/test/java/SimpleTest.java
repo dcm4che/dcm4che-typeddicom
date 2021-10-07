579a7ad5a88283098c86560b84fda4a86423047e
Niklas Roth <36939232+Nirostar@users.noreply.github.com>
2021-10-07 13:07:52 +0200

    Add some example code in the tests

--- typeddicom-lib/src/test/java/SimpleTest.java
+++ typeddicom-lib/src/test/java/SimpleTest.java
@@ -1,9 +1,9 @@
-import com.agfa.typeddicom.dataelements.DisplayedAreaSelectionSequence;
-import com.agfa.typeddicom.dataelements.PresentationPixelAspectRatio;
-import com.agfa.typeddicom.dataelements.PresentationPixelSpacing;
+import com.agfa.typeddicom.dataelements.*;
+import com.agfa.typeddicom.iods.CRImageIOD;
 import com.agfa.typeddicom.iods.GrayscaleSoftcopyPresentationStateIOD;
 import com.agfa.typeddicom.modules.DisplayedAreaModule;
 import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Tag;
 import org.dcm4che3.io.DicomInputStream;
 import org.junit.jupiter.api.Test;
 
@@ -24,12 +24,43 @@ public class SimpleTest {
 
     @Test
     public void testSimpleStuff() throws IOException {
+        // Attributes attributes = readDicomFile("GSPS.dcm");
+        // GrayscaleSoftcopyPresentationStateIOD grayscaleSoftcopyPresentationState = new GrayscaleSoftcopyPresentationStateIOD(attributes);
+        // System.out.println(grayscaleSoftcopyPresentationState.getSOPClassUID().getString());
+        // DisplayedAreaSelectionSequence displayedAreaSelectionSequence = grayscaleSoftcopyPresentationState.getDisplayedAreaSelectionSequence();
+        // DisplayedAreaSelectionSequence.Item displayedAreaSelectionSequenceItem = displayedAreaSelectionSequence.get(0);
+        // PresentationPixelAspectRatio presentationPixelAspectRatio = displayedAreaSelectionSequenceItem.getPresentationPixelAspectRatio();
+        // int[] par = presentationPixelAspectRatio.getInts();
+//
+        // Attributes gsps = readDicomFile("GSPS.dcm");
+        // gsps.getSequence(Tag.DisplayedAreaSelectionSequence);
+//
+        // Attributes crImage = readDicomFile("CRImage.dcm");
+        // crImage.getSequence(Tag.DisplayedAreaSelectionSequence);
+
+
         Attributes attributes = readDicomFile("GSPS.dcm");
-        GrayscaleSoftcopyPresentationStateIOD grayscaleSoftcopyPresentationState = new GrayscaleSoftcopyPresentationStateIOD(attributes);
-        System.out.println(grayscaleSoftcopyPresentationState.getSOPClassUID().getString());
-        DisplayedAreaSelectionSequence displayedAreaSelectionSequence = grayscaleSoftcopyPresentationState.getDisplayedAreaSelectionSequence();
-        DisplayedAreaSelectionSequence.Item displayedAreaSelectionSequenceItem = displayedAreaSelectionSequence.get(0);
-        PresentationPixelAspectRatio presentationPixelAspectRatio = displayedAreaSelectionSequenceItem.getPresentationPixelAspectRatio();
-        int[] par = presentationPixelAspectRatio.getInts();
+
+        GrayscaleSoftcopyPresentationStateIOD gsps = new GrayscaleSoftcopyPresentationStateIOD(attributes);
+        int[] ints1 = gsps.getDisplayedAreaSelectionSequence()
+                .get(0)
+                .getPresentationPixelAspectRatio()
+                .getInts();
+
+        int[] ints2 = attributes.getSequence(Tag.DisplayedAreaSelectionSequence)
+                .get(0)
+                .getInts(Tag.PresentationPixelAspectRatio);
+    }
+
+    private void doMuchStuffWithCrImageAndGSPS(GrayscaleSoftcopyPresentationStateIOD gsps, CRImageIOD crImage) {
+        doStuffWithCrImageAndGSPS(crImage, gsps);
+    }
+
+
+    private void doStuffWithCrImageAndGSPS(ImagerPixelSpacing.Accessors crImage, GrayscaleSoftcopyPresentationStateIOD gsps) {
+        double[] imagerPixelSpacing = crImage.getImagerPixelSpacing().getDoubles();
+        for (DisplayedAreaSelectionSequence.Item item : gsps.getDisplayedAreaSelectionSequence()) {
+            item.getPresentationPixelSpacing().setDoubles(imagerPixelSpacing);
+        }
     }
 }
