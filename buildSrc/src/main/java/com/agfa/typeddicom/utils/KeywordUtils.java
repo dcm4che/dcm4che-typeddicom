c875a5509a62a7763c78c984816282b610b037d7
Niklas Roth <36939232+Nirostar@users.noreply.github.com>
2021-10-29 09:17:52 +0200

    Change KeywordUtils to get key TwelveLeadECGIOD instead of One2LeadECGIOD

--- buildSrc/src/main/java/com/agfa/typeddicom/utils/KeywordUtils.java
+++ buildSrc/src/main/java/com/agfa/typeddicom/utils/KeywordUtils.java
@@ -1,8 +1,44 @@
 package com.agfa.typeddicom.utils;
 
+import java.text.DecimalFormat;
 import java.util.regex.Pattern;
 
 public class KeywordUtils {
+
+    private static final String[] tensNames = {
+            "",
+            " ten",
+            " twenty",
+            " thirty",
+            " forty",
+            " fifty",
+            " sixty",
+            " seventy",
+            " eighty",
+            " ninety"
+    };
+    private static final String[] numNames = {
+            "",
+            " one",
+            " two",
+            " three",
+            " four",
+            " five",
+            " six",
+            " seven",
+            " eight",
+            " nine",
+            " ten",
+            " eleven",
+            " twelve",
+            " thirteen",
+            " fourteen",
+            " fifteen",
+            " sixteen",
+            " seventeen",
+            " eighteen",
+            " nineteen"
+    };
 
     private KeywordUtils() {
     }
@@ -11,6 +47,7 @@ public class KeywordUtils {
      * Remove all invalid characters from a string which are not allowed in a Java class name. It removes "'", "(" and
      * ")" as well as " ", "-" and "/" while Capitalizing the following character to make it camel case. If the first
      * character is a digit it replaces it by its written out word.
+     *
      * @param name the name with character which are not allowed in a class name
      * @return the sanitized name
      * @throws InvalidClassNameException if the resulting identifier does not comply to the Regex [A-Z][a-zA-Z\d_$]*.
@@ -20,10 +57,15 @@ public class KeywordUtils {
         identifier = identifier.replace("'", "");
         identifier = identifier.replace("(", "");
         identifier = identifier.replace(")", "");
-        if (Character.isDigit(identifier.charAt(0))) {
-            String[] numbersInText = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-            identifier = numbersInText[Integer.parseInt(identifier.substring(0, 1))] + identifier.substring(1);
+        StringBuilder numberPrefix = new StringBuilder();
+        while (Character.isDigit(identifier.charAt(0))) {
+            numberPrefix.append(identifier.charAt(0));
+            identifier = identifier.substring(1);
         }
+        if (numberPrefix.length() > 0) {
+            identifier = convert(Long.parseLong(numberPrefix.toString())) + identifier;
+        }
+        identifier = Character.toUpperCase(identifier.charAt(0)) + identifier.substring(1);
         identifier = removeCharactersAndCapitalizeFollowingLetter(identifier, ' ', '-', '/');
         if (!Pattern.matches("[A-Z][a-zA-Z\\d_$]*", identifier)) {
             throw new InvalidClassNameException("Invalid class name " + identifier);
@@ -51,5 +93,79 @@ public class KeywordUtils {
         } else {
             return identifier;
         }
+    }
+
+    private static String convertLessThanOneThousand(int number) {
+        String soFar;
+
+        if (number % 100 < 20) {
+            soFar = numNames[number % 100];
+            number /= 100;
+        } else {
+            soFar = numNames[number % 10];
+            number /= 10;
+
+            soFar = tensNames[number % 10] + soFar;
+            number /= 10;
+        }
+        if (number == 0) return soFar;
+        return numNames[number] + " hundred" + soFar;
+    }
+
+
+    public static String convert(long number) {
+        // 0 to 999 999 999 999
+        if (number == 0) {
+            return "zero";
+        }
+
+        String snumber = Long.toString(number);
+
+        // pad with "0"
+        String mask = "000000000000";
+        DecimalFormat df = new DecimalFormat(mask);
+        snumber = df.format(number);
+
+        // XXXnnnnnnnnn
+        int billions = Integer.parseInt(snumber.substring(0, 3));
+        // nnnXXXnnnnnn
+        int millions = Integer.parseInt(snumber.substring(3, 6));
+        // nnnnnnXXXnnn
+        int hundredThousands = Integer.parseInt(snumber.substring(6, 9));
+        // nnnnnnnnnXXX
+        int thousands = Integer.parseInt(snumber.substring(9, 12));
+
+        String tradBillions;
+        if (billions == 0) {
+            tradBillions = "";
+        } else {
+            tradBillions = convertLessThanOneThousand(billions)
+                    + " billion ";
+        }
+        String result = tradBillions;
+
+        String tradMillions;
+        if (millions == 0) {
+            tradMillions = "";
+        } else {
+            tradMillions = convertLessThanOneThousand(millions)
+                    + " million ";
+        }
+        result = result + tradMillions;
+
+        String tradHundredThousands = switch (hundredThousands) {
+            case 0 -> "";
+            case 1 -> "one thousand ";
+            default -> convertLessThanOneThousand(hundredThousands)
+                    + " thousand ";
+        };
+        result = result + tradHundredThousands;
+
+        String tradThousand;
+        tradThousand = convertLessThanOneThousand(thousands);
+        result = result + tradThousand;
+
+        // remove extra spaces!
+        return result.replaceAll("^\\s+", "").replaceAll("\\b\\s{2,}\\b", " ");
     }
 }
