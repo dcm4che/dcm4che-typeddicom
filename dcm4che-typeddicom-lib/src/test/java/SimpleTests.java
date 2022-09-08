7423f72f86e05e53a8ecf843781369e388bf32cb
Nirostar <36939232+Nirostar@users.noreply.github.com>
2022-09-08 16:12:31 +0200

    Add description of fluent synthesizer API to README.MD

--- dcm4che-typeddicom-lib/src/test/java/SimpleTests.java
+++ dcm4che-typeddicom-lib/src/test/java/SimpleTests.java
@@ -4,31 +4,20 @@ import org.dcm4che.typeddicom.dataelements.ReferencedSeriesSequence;
 import org.dcm4che.typeddicom.iods.CRImageIOD;
 import org.dcm4che.typeddicom.iods.GrayscaleSoftcopyPresentationStateIOD;
 import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Sequence;
 import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.VR;
 import org.dcm4che3.io.DicomInputStream;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
 
-import static org.dcm4che3.data.Tag.ReferencedSeriesSequence;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SimpleTests {
-    @SuppressWarnings("deprecation")
-    private Attributes readDicomFile(String path) throws IOException {
-        Attributes attributes;
-        File dicomFile = new File(SimpleTests.class.getClassLoader().getResource(path).getFile());
-        try (DicomInputStream dicomIn = new DicomInputStream( new FileInputStream( dicomFile ) ))
-        {
-            attributes = dicomIn.readDataset( -1, -1 );
-        }
-        return attributes;
-    }
-
     @Test
     void accessingValuesTheOldFashionedWayShouldYieldTheSameResults() throws IOException {
         Attributes attributes = readDicomFile("GSPS.dcm");
@@ -44,6 +33,16 @@ class SimpleTests {
                 .getInts(Tag.PresentationPixelAspectRatio);
 
         assertArrayEquals(untypedPixelAspectRatio, typedDicomPixelAspectRatio, "Ints must be the same if retrieved by the typed methods");
+    }
+
+    @SuppressWarnings("deprecation")
+    private Attributes readDicomFile(String path) throws IOException {
+        Attributes attributes;
+        File dicomFile = new File(SimpleTests.class.getClassLoader().getResource(path).getFile());
+        try (DicomInputStream dicomIn = new DicomInputStream(new FileInputStream(dicomFile))) {
+            attributes = dicomIn.readDataset(-1, -1);
+        }
+        return attributes;
     }
 
     @Test
@@ -66,7 +65,7 @@ class SimpleTests {
                 .getPatientNameSetter().setString("Roth");
         assertEquals("Roth", gsps.getPatientName().getString());
     }
-    
+
     @Test
     void synthesizingGSPSWithFluentAPICreatesValidGSPS() throws IOException {
         GrayscaleSoftcopyPresentationStateIOD gsps = new GrayscaleSoftcopyPresentationStateIOD()
@@ -78,11 +77,11 @@ class SimpleTests {
                                         new ReferencedImageSequence.Item()
                                                 .getReferencedSOPInstanceUIDSetter().setString("3483648368436.483.864369.43648.368")
                                                 .getReferencedSOPClassUIDSetter().setString("8646.36.54186.86408684371")
-                                                .getReferencedFrameNumberSetter().setInts(1,2,3,4,5),
+                                                .getReferencedFrameNumberSetter().setInts(1, 2, 3, 4, 5),
                                         new ReferencedImageSequence.Item()
                                                 .getReferencedSOPInstanceUIDSetter().setString("782583648368436.478754369.436487827")
                                                 .getReferencedSOPClassUIDSetter().setString("7827287.5634836.8463841.3684.3")
-                                                .getReferencedFrameNumberSetter().setInts(0,1,2,3,4)
+                                                .getReferencedFrameNumberSetter().setInts(0, 1, 2, 3, 4)
                                 )
                 )
                 .appendDisplayedAreaSelectionSequence(
@@ -91,6 +90,31 @@ class SimpleTests {
                                 .getDisplayedAreaBottomRightHandCornerSetter().setInts(1920, 1080)
                                 .getPresentationPixelAspectRatioSetter().setInts(1, 1)
                 );
+
+        Attributes gspsClassic = new Attributes();
+        gspsClassic.setString(Tag.PatientName, VR.PN, "Niklas");
+        Sequence referencedSeriesSequence = gspsClassic.newSequence(Tag.ReferencedSeriesSequence, 1);
+        Attributes referencedSeriesSequenceItem = new Attributes();
+        referencedSeriesSequenceItem.setString(Tag.SeriesInstanceUID, VR.UI, "1234567890.345678.3456789");
+        Sequence referencedInstanceSequence = referencedSeriesSequenceItem.newSequence(Tag.ReferencedImageSequence, 2);
+        Attributes referencedInstanceSequenceItem1 = new Attributes();
+        referencedInstanceSequenceItem1.setString(Tag.ReferencedSOPInstanceUID, VR.UI, "3483648368436.483.864369.43648.368");
+        referencedInstanceSequenceItem1.setString(Tag.ReferencedSOPClassUID, VR.UI, "8646.36.54186.86408684371");
+        referencedInstanceSequenceItem1.setInt(Tag.ReferencedFrameNumber, VR.IS, 1, 2, 3, 4, 5);
+        referencedInstanceSequence.add(referencedInstanceSequenceItem1);
+        Attributes referencedInstanceSequenceItem2 = new Attributes();
+        referencedInstanceSequenceItem2.setString(Tag.ReferencedSOPInstanceUID, VR.UI, "782583648368436.478754369.436487827");
+        referencedInstanceSequenceItem2.setString(Tag.ReferencedSOPClassUID, VR.UI, "7827287.5634836.8463841.3684.3");
+        referencedInstanceSequenceItem2.setInt(Tag.ReferencedFrameNumber, VR.IS, 0, 1, 2, 3, 4);
+        referencedInstanceSequence.add(referencedInstanceSequenceItem2);
+        referencedSeriesSequence.add(referencedSeriesSequenceItem);
+        Sequence displayedAreaSelectionSequence = gspsClassic.newSequence(Tag.DisplayedAreaSelectionSequence, 1);
+        Attributes displayedAreaSelectionSequenceItem = new Attributes();
+        displayedAreaSelectionSequenceItem.setInt(Tag.DisplayedAreaTopLeftHandCorner, VR.SL, 1, 1);
+        displayedAreaSelectionSequenceItem.setInt(Tag.DisplayedAreaBottomRightHandCorner, VR.SL, 1920, 1080);
+        displayedAreaSelectionSequenceItem.setInt(Tag.PresentationPixelAspectRatio, VR.IS, 1, 1);
+        displayedAreaSelectionSequence.add(displayedAreaSelectionSequenceItem);
+        
         assertEquals("Niklas", gsps.getPatientName().getString());
         int[] expectedFrameNumbers = {1, 2, 3, 4, 5};
         assertArrayEquals(expectedFrameNumbers,
@@ -98,5 +122,7 @@ class SimpleTests {
                         .getReferencedImageSequence().get(0)
                         .getReferencedFrameNumber().getInts()
         );
+        
+        assertEquals(gspsClassic, gsps.getAttributes());
     }
 }
