8d033d6652cadc66e0b5e9d291c41eb57ed42b0b
Nirostar <36939232+Nirostar@users.noreply.github.com>
2023-04-14 10:08:29 +0200

    Create default privateDicomMetamodelYamlDirectory

--- dcm4che-typeddicom-generator/dcm4che-typeddicom-java-generator-gradleplugin/src/main/java/org/dcm4che/typeddicom/generator/gradleplugin/TypeddicomJavaGeneratorPlugin.java
+++ dcm4che-typeddicom-generator/dcm4che-typeddicom-java-generator-gradleplugin/src/main/java/org/dcm4che/typeddicom/generator/gradleplugin/TypeddicomJavaGeneratorPlugin.java
@@ -9,14 +9,15 @@ public class TypeddicomJavaGeneratorPlugin implements Plugin<Project> {
     public void apply(final Project target) {
         final TypeddicomJavaGeneratorPluginExtension extension =
                 target.getExtensions().create("generateTypeddicomJavaSources", TypeddicomJavaGeneratorPluginExtension.class);
+        SourceSetContainer sourceSets = (SourceSetContainer) target.getProperties().get("sourceSets");
+        sourceSets.named("main", sourceSet -> extension.getPrivateDicomMetamodelYamlDirectory().convention(sourceSet.getResources().getDestinationDirectory()));
         TaskProvider<GenerateJavaSourcesTask> generateJavaSourcesFileTaskProvider =
                 target.getTasks().register("generateJavaSourceFiles", GenerateJavaSourcesTask.class, task -> {
                     task.getPrivateDicomMetamodelYamlDirectory().set(extension.getPrivateDicomMetamodelYamlDirectory());
                     task.getGeneratedJavaOutputDirectory().set(extension.getGeneratedJavaOutputDirectory());
                 });
         target.getPluginManager().withPlugin("java", javaPlugin -> {
-            SourceSetContainer sourceSets = (SourceSetContainer) target.getProperties().get("sourceSets");
-            sourceSets.getByName("main").getJava().srcDir(generateJavaSourcesFileTaskProvider);
+            sourceSets.named("main", sourceSet -> sourceSet.getJava().srcDir(generateJavaSourcesFileTaskProvider));
             String version = target.getVersion().toString();
             target.getDependencies().add("api", "org.dcm4che:dcm4che-typeddicom-skeleton:" + version);
             target.getDependencies().add("implementation", "org.dcm4che:dcm4che-core");
