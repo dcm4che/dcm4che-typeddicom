aa9c1239edd31c9a93fcae9942b8b97f4f8df4ed
Niklas Roth <36939232+Nirostar@users.noreply.github.com>
2023-04-19 14:01:28 +0200

    Update conventions for gradle generator plugin

--- dcm4che-typeddicom-generator/dcm4che-typeddicom-java-generator-gradleplugin/src/main/java/org/dcm4che/typeddicom/generator/gradleplugin/TypeddicomJavaGeneratorPlugin.java
+++ dcm4che-typeddicom-generator/dcm4che-typeddicom-java-generator-gradleplugin/src/main/java/org/dcm4che/typeddicom/generator/gradleplugin/TypeddicomJavaGeneratorPlugin.java
@@ -9,14 +9,15 @@ public class TypeddicomJavaGeneratorPlugin implements Plugin<Project> {
     public void apply(final Project target) {
         final TypeddicomJavaGeneratorPluginExtension extension =
                 target.getExtensions().create("generateTypeddicomJavaSources", TypeddicomJavaGeneratorPluginExtension.class);
-        SourceSetContainer sourceSets = target.getExtensions().getByType(SourceSetContainer.class);
-        sourceSets.named("main", sourceSet -> extension.getPrivateDicomMetamodelYamlDirectory().convention(target.getLayout().getProjectDirectory().dir("src/main/resources")));
+        extension.getGeneratedJavaOutputDirectory().convention(target.getLayout().getBuildDirectory().dir("typeddicom"));
+        extension.getPrivateDicomMetamodelYamlDirectory().convention(target.getLayout().getProjectDirectory().dir("src/main/resources"));
         TaskProvider<GenerateJavaSourcesTask> generateJavaSourcesFileTaskProvider =
                 target.getTasks().register("generateJavaSourceFiles", GenerateJavaSourcesTask.class, task -> {
                     task.getPrivateDicomMetamodelYamlDirectory().set(extension.getPrivateDicomMetamodelYamlDirectory());
                     task.getGeneratedJavaOutputDirectory().set(extension.getGeneratedJavaOutputDirectory());
                 });
         target.getPluginManager().withPlugin("java-library", javaPlugin -> {
+            SourceSetContainer sourceSets = target.getExtensions().getByType(SourceSetContainer.class);
             sourceSets.named("main", sourceSet -> sourceSet.getJava().srcDir(generateJavaSourcesFileTaskProvider));
             String version = TypeddicomJavaGeneratorPlugin.class.getPackage().getImplementationVersion();
             target.getDependencies().add("api", "org.dcm4che:dcm4che-typeddicom-skeleton:" + version);
