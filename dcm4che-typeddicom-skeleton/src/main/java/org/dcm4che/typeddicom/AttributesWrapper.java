1e834e064431a3448de22b8144354cb3a5cdd45c
Niklas Roth <36939232+Nirostar@users.noreply.github.com>
2023-05-05 16:58:23 +0200

    Add methods for getting the known and unknown tags for wrapped attributes

--- dcm4che-typeddicom-skeleton/src/main/java/org/dcm4che/typeddicom/AttributesWrapper.java
+++ dcm4che-typeddicom-skeleton/src/main/java/org/dcm4che/typeddicom/AttributesWrapper.java
@@ -4,6 +4,11 @@ import org.dcm4che3.data.Attributes;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public interface AttributesWrapper {
     static <T extends AttributesWrapper> T wrap(Attributes attributes, Class<T> wrapperClass) {
@@ -13,6 +18,33 @@ public interface AttributesWrapper {
             return constructor.newInstance(attributes);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new ConstructorNotImplementedException(wrapperClass.getName() + " could not be instantiated with Attributes.", e);
+        }
+    }
+
+    static IntStream getKnownAttributeTags(Class<? extends AttributesWrapper> attributesWrapperClass) {
+        return getImplementedDataElementHolders(attributesWrapperClass)
+                .map(Class::getDeclaringClass)
+                .mapToInt(clazz -> {
+                    try {
+                        return clazz.getField("TAG").getInt(null);
+                    } catch (IllegalAccessException | NoSuchFieldException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    @SuppressWarnings({
+            "unchecked", // It's okay we are checking the type by reflection
+            "java:S1872", // We want a simple name comparison and no instanceof because there are multiple nested classes with that name
+    })
+    static Stream<Class<? extends AttributesWrapper>> getImplementedDataElementHolders(Class<? extends AttributesWrapper> clazz) {
+        if ("Holder".equals(clazz.getSimpleName())) {
+            return Stream.of(clazz);
+        } else {
+            return Arrays.stream(clazz.getInterfaces())
+                    .filter(AttributesWrapper.class::isAssignableFrom)
+                    .map(parent -> (Class<? extends AttributesWrapper>) parent)
+                    .flatMap(AttributesWrapper::getImplementedDataElementHolders);
         }
     }
 
@@ -43,5 +75,17 @@ public interface AttributesWrapper {
      */
     default boolean isEmpty() {
         return getAttributes().isEmpty();
+    }
+
+    default int[] getUnknownAttributeTags() {
+        Set<Integer> knownAttributeTags = getKnownAttributeTags(this.getClass())
+                .boxed()
+                .collect(Collectors.toUnmodifiableSet());
+        return Arrays.stream(this.getAttributes().tags())
+                .filter(tag -> !knownAttributeTags.contains(tag)).toArray();
+    }
+
+    default int[] getKnownAttributeTags() {
+        return getKnownAttributeTags(this.getClass()).toArray();
     }
 }
